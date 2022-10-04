4d1769f2cdbecea935353311af385db99079bee5
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2022-10-04 09:48:37 +0900

    タイムライン画面で表示させる2つのパターンのタイムライン画面の枠組みを作成

---
+++ lib/view/timeline/interest_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InterestScreen extends StatelessWidget {
+  const InterestScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('気になるタイムライン'),
+    );
+  }
+}
