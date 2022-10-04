2f804d41ab493d44e4c77e1e1265fe48bab7041e
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2022-10-04 14:42:33 +0900

    interest_hobby_cardの編集

--- lib/view/timeline/children/interest_hobby_card.dart
+++ lib/view/timeline/children/interest_hobby_card.dart
@@ -11,35 +11,52 @@ class InterestHobbyCard extends StatelessWidget {
       child: Stack(
         children: [
           Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/back_image.jpeg'),
-                fit: BoxFit.cover,
-              ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.lightBlueAccent,
             ),
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [0.5, 0.7, 0.95],
-                  colors: [
-                    Colors.black12,
-                    Colors.black54,
-                    Colors.black87,
-                  ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(1.5),
+            child: Column(
+              children: [
+                const Center(
+                  child: Text(
+                    'ユーザA',
+                    style: TextStyle(fontSize: 12, color: Colors.white),
+                  ),
                 ),
-              ),
+                Container(
+                  height: 278,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: const DecorationImage(
+                      image: AssetImage('assets/images/back_image.jpeg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      gradient: const LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.5, 0.7, 0.95],
+                        colors: [
+                          Colors.black12,
+                          Colors.black54,
+                          Colors.black87,
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Column(
             children: const [
-              ColoredBox(
-                color: Colors.blueAccent,
-                //TODO firebaseから取ってくる
-                child: Text('ユーザーネーム'),
-              ),
-              SizedBox(height: 120),
+              SizedBox(height: 150),
               //TODO firebaseから取ってくる
               CircleAvatar(
                 radius: 40,
@@ -48,7 +65,10 @@ class InterestHobbyCard extends StatelessWidget {
               //TODO firebaseから取ってくる
               Text(
                 '今日は京都の嵐山でサイクリングに行ってきました!!',
-                style: TextStyle(color: Colors.white),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
               ),
