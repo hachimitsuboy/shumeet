639b721a42a61e5a9d7b458583b4132a57ac767c
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2022-12-03 18:04:50 +0900

    ProfileStatusWidgetを画面に表示

--- lib/view/profile/profile_screen.dart
+++ lib/view/profile/profile_screen.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:shumeet/view/profile/children/profile_status.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('プロフィール画面'),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        SizedBox(height: 45),
+        Padding(
+          padding: EdgeInsets.only(left: 16.0),
+          child: Text(
+            'はちみつぼーい',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        SizedBox(height: 15),
+        ProfileStatus(),
+      ],
     );
   }
 }
